ee306efa2f525bde7ab0295a1318b9aa006b9ce5
Alessandro Conti <alessandro.conti.97@gmail.com>
2022-10-07 12:23:21 +0200

    Remove residue of benchmark datasets

--- cli/commands/data_prepare.py
+++ cli/commands/data_prepare.py
@@ -3,7 +3,7 @@
 from itertools import product
 
 from cli.commands.base import BaseCommand
-from src.datasets import BENCHMARK_DATASETS, DATASETS
+from src.datasets import DATASETS
 
 
 class DataPrepare(BaseCommand):
@@ -22,8 +22,8 @@ class DataPrepare(BaseCommand):
 
         domains = dataset.available_domains if len(dataset.available_domains) > 0 else [""]
         combinations = product(domains, dataset.available_splits)
-        for i, (domain, split) in enumerate(combinations):
-            download = self.options.get("dataset") in BENCHMARK_DATASETS and i == 0
+        for _, (domain, split) in enumerate(combinations):
+            download = False
             if domain == "":
                 dataset(split=split, prepare=True, download=download, **kwargs)
             else:
